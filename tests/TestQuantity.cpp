24134d9785f5a8873c8ebc87f7be5fc2d41d9e2b
piotr.idzik <vil02@o2.pl>
2022-11-17 23:58:31 +0100

    tests: add has_same_size_as_value_type

--- tests/TestQuantity.cpp
+++ tests/TestQuantity.cpp
@@ -1,10 +1,11 @@
-#define BOOST_TEST_MODULE TestTypeConversionUtils
+#define BOOST_TEST_MODULE TestQuantity
 
 #include "../CtUnits/ctu/Quantity.hpp"
 #include <boost/mp11.hpp>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/static_assert.hpp>
 #include <type_traits>
+#include <cstdint>
 
 BOOST_AUTO_TEST_CASE(test_get_value)
 {
@@ -128,4 +129,17 @@ BOOST_AUTO_TEST_CASE(test_divide)
         std::is_same_v<std::remove_const_t<decltype(result)>,
         expected_result_type>);
     BOOST_CHECK_EQUAL(result.get_value(), value_a/value_b);
+}
+
+using value_types = boost::mp11::mp_list<std::int8_t, std::int16_t, std::int32_t, std::int64_t>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(has_same_size_as_value_type, value_type, value_types)
+{
+      using units_dimensions = boost::mp11::mp_list<
+          ctu::UnitDimension<bool, -1>,
+          ctu::UnitDimension<char, 2>,
+          ctu::UnitDimension<double, 3>,
+          ctu::UnitDimension<int, -23> >;
+      BOOST_STATIC_ASSERT(
+          sizeof(ctu::Quantity<value_type, units_dimensions>) == sizeof(value_type));
 }
