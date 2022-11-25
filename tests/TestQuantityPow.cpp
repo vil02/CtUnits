1f28f93dc893815e501c950c5cceef046217f481
piotr.idzik <vil02@o2.pl>
2022-11-25 17:27:26 +0100

    style: use  in long double literals

--- tests/TestQuantityPow.cpp
+++ tests/TestQuantityPow.cpp
@@ -11,7 +11,7 @@ using ExampleUnitDim = ctu::UdMap<
     ctu::UnitDimension<bool, 1>, ctu::UnitDimension<char, -3>,
     ctu::UnitDimension<int, 5>>;
 
-const auto example_quantity = ctu::Quantity<long double, ExampleUnitDim>(4.0);
+const auto example_quantity = ctu::Quantity<long double, ExampleUnitDim>(4.0L);
 
 BOOST_AUTO_TEST_CASE(TestPowWithNaturalPower)
 {
@@ -19,7 +19,7 @@ BOOST_AUTO_TEST_CASE(TestPowWithNaturalPower)
         long double,
         ctu::UdMap<
             ctu::UnitDimension<bool, 2>, ctu::UnitDimension<char, -6>,
-            ctu::UnitDimension<int, 10>>>(16.0);
+            ctu::UnitDimension<int, 10>>>(16.0L);
     const auto actual = example_quantity.pow<std::ratio<2>>();
     static_assert(std::is_same_v<decltype(actual), decltype(expected)>);
     BOOST_TEST(actual.get_value() == expected.get_value());
@@ -31,7 +31,7 @@ BOOST_AUTO_TEST_CASE(TestPowWithFractionalPower)
         long double,
         ctu::UdMap<
             ctu::UnitDimension<bool, 1, 2>, ctu::UnitDimension<char, -3, 2>,
-            ctu::UnitDimension<int, 5, 2>>>(2.0);
+            ctu::UnitDimension<int, 5, 2>>>(2.0L);
     const auto actual = example_quantity.pow<std::ratio<1, 2>>();
     static_assert(std::is_same_v<decltype(actual), decltype(expected)>);
     BOOST_TEST(actual.get_value() == expected.get_value());
@@ -39,7 +39,7 @@ BOOST_AUTO_TEST_CASE(TestPowWithFractionalPower)
 
 BOOST_AUTO_TEST_CASE(TestPowWithZeroPower)
 {
-    const auto expected = ctu::Quantity<long double, ctu::UdMap<>>(1.0);
+    const auto expected = ctu::Quantity<long double, ctu::UdMap<>>(1.0L);
     const auto actual = example_quantity.pow<std::ratio<0>>();
     static_assert(std::is_same_v<decltype(actual), decltype(expected)>);
     BOOST_TEST(actual.get_value() == expected.get_value());
